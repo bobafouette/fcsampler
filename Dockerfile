462f4def8384ce2d2c65012ebfafd5b8b94e5c41
loup_kreidl <loup.kreidl@gmail.com>
2021-03-21 20:16:42 +0100

    Update fcsampler

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
 FROM python:3
 
-RUN pip install discord.py
-RUN apt-get update
-RUN apt-get install ffmpeg -y
-RUN pip install PyNaCl
+RUN apt-get update && \
+    apt-get install ffmpeg -y && \
+    pip install discord.py && \
+    pip install PyNaCl && \
+    pip install flask
 
-COPY ./bot.py work/
-RUN mkdir /work/ressources
-COPY ./token.secret work/ressources/
-COPY ./audio work/ressources/audio/
+RUN mkdir /opt/fcsampler
+WORKDIR /opt/fcsampler
+COPY ./bot.py ./
+COPY ./templates ./templates
+COPY ./static ./static
+COPY ./token.secret ./ressources/
+COPY ./audio ./ressources/audio/
+
 # VOLUME /work/ressources
-ENV SOUND_PATH '/work/ressources/audio'
-ENV TOKEN_PATH '/work/ressources/token.secret'
-CMD python work/bot.py bot
+ENV SOUND_PATH '/opt/fcsampler/ressources/audio'
+ENV TOKEN_PATH '/opt/fcsampler/ressources/token.secret'
+ENV TEMPLATES_PATH '/opt/fcsampler/templates/'
+EXPOSE 5000
+CMD python ./bot.py bot
--- bot.py
+++ bot.py
@@ -8,8 +8,13 @@ from discord.ext import commands
 from flask import Flask
 from flask import render_template
 from flask import Response
+from flask import flash, request, redirect, url_for
 from functools import partial
 from threading import Thread
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.environ.get('SOUND_PATH')
+ALLOWED_EXTENSIONS = {'mp3'}
 
 YADUMONDE = 240872092965404693
 BOTMUSIC = 690959439842508863
@@ -17,11 +22,15 @@ LAGALETTE = 240974139572224010
 
 class Sample(object):
 
+    @staticmethod
+    def index_match(filename):
+        index_regex = re.compile(r"(?P<index>[0-9]+)\s(?P<shortname>.+)")
+        return re.match(index_regex, filename)
+
     def __init__(self, name, path):
         self.name = name
         self.path = path
-        index_regex = re.compile(r"(?P<index>[0-9]+)\s(?P<shortname>.+)")
-        index_match = re.match(index_regex, self.name)
+        index_match = self.index_match(self.name)
         if not index_match:
             raise ValueError('Not a valid sound name: {}. Could not extract index'.format(self.name))
         self.index = int(index_match.group('index'))
@@ -55,6 +64,15 @@ class JukeBox(object):
         if not os.path.isdir(sounds_path):
             raise ValueError('{} is not a folder or does not exists'.format(self.sounds_path))
 
+        self.load()
+
+    def getlist(self):
+        return [sound.name for sound in self.playlist]
+
+    def __getitem__(self, index):
+        return self.playlist[index-1]
+
+    def load(self):
         self.playlist = []
         sounds_files = os.listdir(self.sounds_path)
         for i in range(len(sounds_files)):
@@ -65,12 +83,6 @@ class JukeBox(object):
             self.playlist.append(sample)
 
         self.playlist.sort()
-
-    def getlist(self):
-        return [sound.name for sound in self.playlist]
-
-    def __getitem__(self, index):
-        return self.playlist[index-1]
 
 class FcSampler(commands.Bot):
 
@@ -142,6 +154,7 @@ def bot_main():
     fcSampler.run(token)
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
@@ -149,7 +162,6 @@ def hello_world():
 
 @app.route('/play/<index>')
 def play_web(index=1):
-    print('play')
     sample = jukebox[int(index)]
     try:
         fcSampler.play(sample)
@@ -157,8 +169,43 @@ def play_web(index=1):
         return Response('Already playing audio', status=409)
     return Response('Playing Sound {}'.format(sample.shortname), status=202)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/reload')
+def reload():
+    jukebox.load()
+    return redirect(url_for('hello_world'))
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            match_filename = Sample.index_match(filename)
+            if match_filename:
+                index = match_filename.group('index')
+                if index == len(jukebox.playlist):
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    return
+                filename = match_filename.group('shortname')
+            filename = str(len(jukebox.playlist) + 1) + ' ' + filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            jukebox.load()
+            return redirect(url_for('hello_world'))
+
 partial_run = partial(app.run, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
 t = Thread(target=partial_run)
 t.start()
 bot_main()
-
